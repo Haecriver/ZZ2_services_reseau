ea7039ca32ab2df9f724ae8734ed80cd756aee0c
theCmaker <pierre-loup.pissavy@poste.isima.fr>
2016-02-04 18:44:32 +0100

    Push Anne-Lise

--- JediTournamentWPF/Models/MatchModel.cs
+++ JediTournamentWPF/Models/MatchModel.cs
@@ -1,6 +1,7 @@
 ﻿using BiblioWPF.ViewModel;
 using EntitiesLayer;
 using JediTournamentWPF.ListModels;
+using stadeTournamentWPF.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -28,6 +29,8 @@ namespace JediTournamentWPF.Models
                     match.JediVainqueur = value.Jedi;
                 }
                 OnPropertyChanged("JediVainqueur");
+                OnPropertyChanged("Jedi1Win");
+                OnPropertyChanged("Jedi2Win");
             }
         }
 
@@ -36,7 +39,7 @@ namespace JediTournamentWPF.Models
             get { return new JediModel(match.Jedi1); }
             set
             {
-                if (value != null)
+                if (value != null && value!=Jedi2)
                 {
                     match.Jedi1 = value.Jedi;
                 }
@@ -49,7 +52,7 @@ namespace JediTournamentWPF.Models
             get { return new JediModel(match.Jedi2); }
             set
             {
-                if (value != null)
+                if (value != null && value!=Jedi1)
                 {
                     match.Jedi2 = value.Jedi;
                 }
@@ -57,26 +60,88 @@ namespace JediTournamentWPF.Models
             }
         }
 
+        public StadeModel Stade
+        {
+            get { return new StadeModel(match.Stade); }
+            set
+            {
+                if (value != null)
+                {
+                    match.Stade = value.Stade;
+                }
+                OnPropertyChanged("Stade");
+            }
+        }
+ 
         public EPhaseTournoi PhaseTournoi
         {
             get { return match.PhaseTournoi; }
             set
             {
+                if (this.PhaseTournoi == value)
+                    return;
+
                 match.PhaseTournoi = value;
                 OnPropertyChanged("PhaseTournoi");
+                OnPropertyChanged("IsFinale");
+                OnPropertyChanged("IsQuart");
+                OnPropertyChanged("IsHuitieme");
+                OnPropertyChanged("IsDemi");
             }
         }
 
-        public Stade Stade
+        /*******************************************Pour les boutons radio**********************************************/
+        public bool IsFinale
         {
-            get { return match.Stade; }
+            get { return PhaseTournoi == EPhaseTournoi.Finale; }
+            set { 
+               PhaseTournoi= value ? match.PhaseTournoi=EPhaseTournoi.Finale : PhaseTournoi; 
+            }
+        }
+ 
+        public bool IsHuitieme
+        {
+           get { return PhaseTournoi == EPhaseTournoi.HuitiemeFinale; }
+            set { 
+                PhaseTournoi= value ? match.PhaseTournoi=EPhaseTournoi.HuitiemeFinale : PhaseTournoi; 
+            }
+        }
+ 
+        public bool IsDemi
+        {
+            get { return PhaseTournoi == EPhaseTournoi.DemiFinale; }
+            set { 
+                PhaseTournoi= value ? match.PhaseTournoi=EPhaseTournoi.DemiFinale : PhaseTournoi; 
+            }
+        }
+
+        public bool IsQuart
+        {
+            get { return PhaseTournoi == EPhaseTournoi.QuartFinale; }
+            set { 
+                PhaseTournoi= value ? match.PhaseTournoi=EPhaseTournoi.QuartFinale : PhaseTournoi; 
+            }
+        }
+
+        public bool Jedi1Win
+        {
+            get { return JediVainqueur.Jedi==Jedi1.Jedi; }
             set
             {
-                match.Stade = value;
-                OnPropertyChanged("Stade");
+                JediVainqueur = value ? JediVainqueur=Jedi1 : JediVainqueur;
             }
         }
 
+        public bool Jedi2Win
+        {
+            get { return JediVainqueur.Jedi==Jedi2.Jedi; }
+            set
+            {
+                JediVainqueur = value ? JediVainqueur=Jedi2 : JediVainqueur;
+            }
+        }
+
+        /**********************To string***********************/
         public override String ToString()
         {
             return match.ToString();
